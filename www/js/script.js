d36141d8686b399b268746a02a37df42611d96f2
Glomberg <Bazz@bk.ru>
2015-09-23 10:54:14 +0300

    123

--- www/js/script.js
+++ www/js/script.js
@@ -1,1 +1,8 @@
-﻿
+﻿$(document).ready(function(){
+	$("a.scroll").on("click", function(){
+		href = $(this).attr("href");
+		if($(href).length != 0) {
+			$("body,html").animate({ scrollTop: $(href).offset().top }, 1000);
+		}
+	});
+)};
